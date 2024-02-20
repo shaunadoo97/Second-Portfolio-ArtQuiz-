8d67204a297a7a5aeefc7c89b6cad3eeaf5f99ca
Shauna Hathaway McHugh <shaunadoo97@gmail.com>
2024-02-20 18:26:09 +0000

    Currently trying to sort out the issue found in console that question is unefined

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 const startButton = document.getElementById("start-btn")
-const nextButton = document.getElementById("answer-btns")
+const nextButton = document.getElementById("next-btn")
 const questionContainerElement = document.getElementById("quiz")
 const questionElement = document.getElementById("question")
 const answerButtonElement = document.getElementById("answer-btns")
 
-let shuffledQuestions, currentQuestionIndex 
+let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener("click", startGame)
+nextButton.addEventListener("click", () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
 
 function startGame() {
- console.log("Started!")
- startButton.classList.add("hide")
- shuffledQuestions = questions.sort(() => Math.random() - .5)
- currentQuestionIndex = 0
- questionContainerElement.classList.remove("hide")
- setNextQuestion()
+    startButton.classList.add("hide")
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove("hide")
+    setNextQuestion()
 }
 
 function setNextQuestion() {
@@ -25,7 +28,7 @@ function setNextQuestion() {
 
 function showQuestion(question) {
     questionElement.innerText = question.question
-    question.answers.forEach ( answer => {
+    question.answers.forEach(answer => {
         const button = document.createElement("btn")
         button.innerText = answer.text
         button.classList.add("btn")
@@ -35,7 +38,6 @@ function showQuestion(question) {
         button.addEventListener('click', selectAnswer)
         answerButtonElement.appendChild(button)
     })
-
 }
 
 function resetState() {
@@ -43,18 +45,42 @@ function resetState() {
     while (answerButtonElement.firstChild) {
         answerButtonElement.removeChild(answerButtonElement.firstChild)
     }
+
 }
+
 
 function selectAnswer(e) {
-
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    nextButton.classList.remove("hide")
 }
 
-const questions = [
-    {
-        question: 'What is 2 = 2', 
-        answers: [ 
-            { text: "4", correct: true },
-            { text: "22", correct: false }
-        ]
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add("correct")
+    } else {
+        element.classList.add("wrong")
     }
-]
+}
+function clearStatusClass(element) {
+    element.classList.remove("correct")
+    element.classList.remove("wrong")
+}
+
+const questions = [{
+    question: 'What is 2 + 2 = ?',
+    answers: [{
+            text: "4",
+            correct: true
+        },
+        {
+            text: "22",
+            correct: false
+        }
+    ]
+}]
