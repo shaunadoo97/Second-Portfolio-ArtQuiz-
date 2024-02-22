1a0adc442b2dac995ae842d7140a5e554843fbbb
Shauna Hathaway McHugh <shaunadoo97@gmail.com>
2024-02-22 19:54:50 +0000

    Adding some content on my README

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 Welcome to my Art Quiz Test!
+I've made a game quiz to test your knowledge on Art. 
+
+## Wireframes 
+Here are my wireframes:
+
+<img src="assets/documentation/start_page.png" width="400" height="200">
+<img src="assets/documentation/playing_page.png" width="400" height="200">
+<img src="assets/documentation/thanks.png" width="400" height="200">
+
+
+## Credits
+### Codes so far 
+I've used the css layout from https://www.youtube.com/watch?v=PBcqGxrr9g8&t=1396s
+Guidance on Javascript using https://www.youtube.com/watch?v=riDzcEQbX6k
--- script.js
+++ script.js
@@ -1,23 +1,27 @@
-const startButton = document.getElementById("start-btn")
-const nextButton = document.getElementById("next-btn")
-const questionContainerElement = document.getElementById("quiz")
-const questionElement = document.getElementById("question")
-const answerButtonElement = document.getElementById("answer-btns")
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const startTest = document.getElementById('test')
+const questionContainerElement = document.getElementById('quiz')
+
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-btns') 
 
 let shuffledQuestions, currentQuestionIndex
 
-startButton.addEventListener("click", startGame)
-nextButton.addEventListener("click", () => {
+startButton.addEventListener('click', gameStart)
+nextButton.addEventListener('click', ()=> {
     currentQuestionIndex++
     setNextQuestion()
 })
 
-function startGame() {
-    startButton.classList.add("hide")
+function gameStart() {
+    startTest.classList.add('hide')
+    startButton.classList.add('hide')
     shuffledQuestions = questions.sort(() => Math.random() - .5)
-    currentQuestionIndex = 0
-    questionContainerElement.classList.remove("hide")
+    currentQuestionIndex = 0 
+    questionContainerElement.classList.remove('hide')
     setNextQuestion()
+
 }
 
 function setNextQuestion() {
@@ -29,77 +33,86 @@ function setNextQuestion() {
 function showQuestion(question) {
     questionElement.innerText = question.question
     question.answers.forEach(answer => {
-        const button = document.createElement("btn")
+        const button = document.createElement('button')
         button.innerText = answer.text
-        button.classList.add("btn")
+        button.classList.add('btn')
         if (answer.correct) {
             button.dataset.correct = answer.correct
         }
         button.addEventListener('click', selectAnswer)
-        answerButtonElement.appendChild(button)
+        answerButtonsElement.appendChild(button)
     })
 }
 
 function resetState() {
-    nextButton.classList.add("hide")
-    while (answerButtonElement.firstChild) {
-        answerButtonElement.removeChild(answerButtonElement.firstChild)
+    clearStatusClass(document.body)
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild
+        (answerButtonsElement.firstChild)
     }
-
 }
 
-
 function selectAnswer(e) {
-    const selectedButton = e.target
-    const correct = selectedButton.dataset.correct
-    setStatusClass(document.body, correct)
-    Array.from(answerButtonElement.children).forEach(button => {
-        setStatusClass(button, button.dataset.correct)
-    })
-    if (shuffledQuestions.length > currentQuestionIndex + 1) {
-        nextButton.classList.remove("hide")
-    } else {
-        startButton.innerText = "Restart"
-    }
-    nextButton.classList.remove("hide")
+ const selectedButton = e.target
+ const correct = selectedButton.dataset.correct
+ setStatusClass(document.body, correct)
+ Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+ })
+ if(shuffledQuestions.length > currentQuestionIndex + 1 ) {
+    nextButton.classList.remove('hide')
+ } else {
+    startButton.innerText = "Restart"
+    startButton.classList.remove("hide")
+ }
+ 
 }
 
 function setStatusClass(element, correct) {
     clearStatusClass(element)
     if (correct) {
-        element.classList.add("correct")
+        element.classList.add('correct')
     } else {
-        element.classList.add("wrong")
+        element.classList.add('wrong')
     }
 }
 
 function clearStatusClass(element) {
-    element.classList.remove("correct")
-    element.classList.remove("wrong")
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
 }
 
-const questions = [{
-    question: 'What is 2 + 2 = ?',
-    answers: [{
-            text: "4",
-            correct: true
-        },
-        {
-            text: "22",
-            correct: false
-        }
-    ],
+const questions = [
+    {
+        question: 'What is 2 + 2 =?',
+        answers: [
+            { text: '4', correct: true},
+            {text: '22', correct: false},
+            {text: '3', correct: false},
+            {text: '5', correct: false},
+        ]
+        
+    },
+    {
+        question: 'What is 4 + 2 =?',
+        answers: [
+            { text: '4', correct: false},
+            {text: '6', correct: true},
+            {text: '8', correct: false},
+            {text: '9', correct: false},
+        ]
+        
+    },
 
-    question: "What is the largest animal",
-    answers: [{
-            text: "Lion",
-            correct: true
-        },
-
-        {
-            text: "mouse",
-            correct: false
-        }
-    ],
-
-}]
+    {
+        question: 'What is 10 + 2 =?',
+        answers: [
+            { text: '17', correct: false},
+            {text: '6', correct: false},
+            {text: '12', correct: true},
+            {text: '2', correct: false},
+        ]
+        
+    }
+]
