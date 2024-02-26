a68d53b58dc87bf3338c8db7e1a51653328bbe71
Shauna Hathaway McHugh <shaunadoo97@gmail.com>
2024-02-26 18:19:09 +0000

    Adding in art questions

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const startButton = document.getElementById('start-btn')
 const nextButton = document.getElementById('next-btn')
 const startTest = document.getElementById('test')
 const questionContainerElement = document.getElementById('quiz')
-
 const questionElement = document.getElementById('question')
 const answerButtonsElement = document.getElementById('answer-btns') 
+
 
 let shuffledQuestions, currentQuestionIndex
 
@@ -15,7 +15,7 @@ nextButton.addEventListener('click', ()=> {
 })
 
 function gameStart() {
-    startTest.classList.add('hide')
+   
     startButton.classList.add('hide')
     shuffledQuestions = questions.sort(() => Math.random() - .5)
     currentQuestionIndex = 0 
@@ -54,11 +54,12 @@ function resetState() {
 }
 
 function selectAnswer(e) {
- const selectedButton = e.target
- const correct = selectedButton.dataset.correct
+ const selectedButton = e.target;
+ const correct = selectedButton.dataset.correct;
  setStatusClass(document.body, correct)
  Array.from(answerButtonsElement.children).forEach(button => {
-    setStatusClass(button, button.dataset.correct)
+    button.removeEventListener("click", selectAnswer);
+    setStatusClass(button, button.dataset.correct);
  })
  if(shuffledQuestions.length > currentQuestionIndex + 1 ) {
     nextButton.classList.remove('hide')
@@ -72,9 +73,9 @@ function selectAnswer(e) {
 function setStatusClass(element, correct) {
     clearStatusClass(element)
     if (correct) {
-        element.classList.add('correct')
+        element.classList.add("correct");
     } else {
-        element.classList.add('wrong')
+        element.classList.add("wrong");
     }
 }
 
@@ -85,34 +86,43 @@ function clearStatusClass(element) {
 
 const questions = [
     {
-        question: 'What is 2 + 2 =?',
+        question: 'Which artist is famous for his series of paintings featuring water lilies?',
         answers: [
-            { text: '4', correct: true},
-            {text: '22', correct: false},
-            {text: '3', correct: false},
-            {text: '5', correct: false},
+            { text: 'Claude Monet', correct: true},
+            {text: ' Diego Velázquez', correct: false},
+            {text: 'Peter Paul Rubens', correct: false},
+            {text: 'Caravaggio', correct: false},
         ]
         
     },
     {
-        question: 'What is 4 + 2 =?',
+        question: 'Who painted the famous "Mona Lisa"?',
         answers: [
-            { text: '4', correct: false},
-            {text: '6', correct: true},
-            {text: '8', correct: false},
-            {text: '9', correct: false},
+            { text: 'Donatello', correct: false},
+            {text: 'Leonardo da Vinci', correct: true},
+            {text: 'Michelangelo', correct: false},
+            {text: 'Raphael', correct: false},
         ]
         
     },
 
     {
-        question: 'What is 10 + 2 =?',
+        question: 'Which artist famously cut off his own ear?',
         answers: [
-            { text: '17', correct: false},
-            {text: '6', correct: false},
-            {text: '12', correct: true},
-            {text: '2', correct: false},
+            { text: 'Rembrandt', correct: false},
+            {text: 'Pablo Picasso', correct: false},
+            {text: 'Vincent Van Gogh', correct: true},
+            {text: 'Johannes Vermeer', correct: false},
         ]
         
     }
 ]
+const myTimeout = setTimeout(gameOver, 10000);
+
+function gameOver() {
+    document.getElementById("timer").innerHTML = "Time ran out!"
+}
+
+function myStopFunction() {
+    clearTimeout(myTimeout);
+}
